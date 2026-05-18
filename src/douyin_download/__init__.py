195b461dc35c84ba29674cccac0e9de27d0a876f
Kim Hsiao <white.shopping@gmail.com>
2026-05-18 12:29:17 +0800

    feat: export URL normalizer functions

--- src/douyin_download/__init__.py
+++ src/douyin_download/__init__.py
@@ -1,3 +1,23 @@
 """Douyin video downloader."""
 
 __version__ = "0.1.0"
+
+from douyin_download.url_normalizer import (
+    normalize,
+    extract_url_from_text,
+    extract_video_id,
+    resolve_short_url,
+    validate_video_id,
+    InvalidURLError,
+    VideoUnavailableError,
+)
+
+__all__ = [
+    "normalize",
+    "extract_url_from_text",
+    "extract_video_id",
+    "resolve_short_url",
+    "validate_video_id",
+    "InvalidURLError",
+    "VideoUnavailableError",
+]
